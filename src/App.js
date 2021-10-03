73e7b0e744fa98f922e23e0a1e727b87793c661a
anastasiakounoupi <anastasia.kunupi@gmail.com>
2021-10-03 19:12:40 +0300

    Finalizing the jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import jumboData from './fixtures/jumbo';
-import Jumbotron from './components/jumbotron';
+import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-      {jumboData.map(item => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <p>{item.title}</p>
-          <p>{item.subtitle}</p>
-          <p>{item.image}</p>
-          <p>{item.alt}</p>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />
 }
